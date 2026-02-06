629ab1b099d14d2b174b90c1f23c837bc691979e
Vardan Gyurjyan <gurjyan@jlab.org>
2026-02-06 13:39:09 -0500

    updated env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 source "$(pwd)/../root/bin/thisroot.sh"
 export CODA=`pwd`/coda
+export CODA_BIN=${CODA}/${MACHINE}/bin
+export CODA_LIB=${CODA}/${MACHINE}/lib
+
 export ERSAP_HOME=`pwd`/ersap
 export ERSAP_USER_DATA=`pwd`/ersap-data
+
+export PATH=${CODA_BIN}:${CODA}/common/bin:${ERSAP_HOME}/bin:${PATH}
